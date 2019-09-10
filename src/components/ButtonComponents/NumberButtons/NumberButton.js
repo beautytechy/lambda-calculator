957b2ee57045f6a3bcb659a7365ccb118ef29cd0
Nicole Hollis <nicole@emmabeauty.co>
2019-09-10 16:24:48 -0700

    Added Number button component

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import Numbers from "./Numbers";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+    <button className="numberButtons">
+      {props.numbers}
+      </button>
     </>
   );
 };
+
+export default NumberButton
